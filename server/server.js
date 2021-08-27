cba854cc46cf1eb100b7c7ffda6d502d5b33f868
Amogh Kapoor <chatur.chaturx@gmail.com>
2021-08-27 18:05:38 +0530

    updates

--- server/server.js
+++ server/server.js
@@ -15,52 +15,42 @@ app.post('/', function (req, res) {
 
 app.post("/refresh", (req, res) => {
     const refreshToken = req.body.refreshToken
-
     var spotifyApi = new SpotifyWebApi({
         clientId: "929c5579854140cf8bea0a5bcf923eeb",
         clientSecret: "5d8565d168ef4d138737d5836ebe167b",
         redirectUri: 'http://localhost:3000/',
         refreshToken
     });
-
     spotifyApi.refreshAccessToken()
         .then(data => {
             res.json({
                 accessToken: data.body.access_token,
                 expiresIn: data.body.expires_in,
             })
-
             spotifyApi.setAccessToken(data.body['access_token']);
-
         })
-        .catch(err => console.log(err))
+        .catch(err => console.error("Refresh Access Token", err))
 })
 
 app.post("/login", (req, res) => {
     var code = req.body.code
-
     var spotifyApi = new SpotifyWebApi({
         clientId: "929c5579854140cf8bea0a5bcf923eeb",
         clientSecret: "5d8565d168ef4d138737d5836ebe167b",
         redirectUri: 'http://localhost:3000'
     });
-
     spotifyApi.authorizationCodeGrant(code)
         .then((data) => {
-
             res.json({
                 accessToken: data.body.access_token,
                 refreshToken: data.body.refresh_token,
                 expiresIn: data.body.expires_in,
             })
-
             spotifyApi.setAccessToken(data.body['access_token']);
             spotifyApi.setRefreshToken(data.body['refresh_token']);
-
         })
         .catch(err => {
             res.sendStatus(400)
-            console.log("server", err);
+            console.error("Code Grant Authorization Error", err);
         })
-
 })
