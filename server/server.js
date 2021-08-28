9b73a9c51ff3041ba739604baaa8ee930fe8d0e6
Amogh Kapoor <chatur.chaturx@gmail.com>
2021-08-28 11:14:34 +0530

    Hosting

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express")
 const cors = require("cors")
 const SpotifyWebApi = require("spotify-web-api-node")
+const path = require("path")
 
 const app = express()
 app.use(cors())
@@ -9,6 +10,10 @@ app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static("../client/build"))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
 }
 
 const PORT = process.env.PORT || 3001
